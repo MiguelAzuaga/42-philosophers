32bbd3b53be90a183a4675c7633c787e3c527c2e
Miguel Queiros Ribeiro Azuaga <miguel.q.azuaga@gmail.com>
2025-08-05 20:32:01 +0100

    slowly but surely

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -4,15 +4,31 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <pthread.h>
+
+# define MALLOC 42
+# define MUTEX 43
+# define USAGE 1
+# define INVALID_ARGS 2
+
+typedef struct s_data
+{
+	int				qty_philo;
+	int				t_die;
+	int				t_eat;
+	int				t_sleep;
+	int				qty_eat;
+	long			start_time;
+	pthread_mutex_t	mutex;
+}	t_data;
 
 typedef struct s_philo
 {
-	int	philo_qty;
-	int	die_t;
-	int	eat_t;
-	int	sleep_t;
-	int	eat_qty;
+	int				id;
+	long			last_eat;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
+	t_data			data;
 }	t_philo;
-
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -1,10 +1,36 @@
 #include "../includes/philo.h"
+#include <sys/time.h>
 
-int	ft_error(int code)
+void	erro_msg(int error)
 {
-	if (code == 42)
+	if (error == MALLOC)
 		write(2, "Malloc error\n", 13);
-	exit(1);
+	else if (error == USAGE)
+		write(2, "Usage:./philo <number_of_philosophers> <time_to_die> <time_\
+to_eat> <time_to_sleep> [number_of_times_each_philosopher_must_eat]\n", 127);
+	else if (error == INVALID_ARGS)
+		write(2, "Invalid arguments\n", 18);
+	else if (error == MUTEX)
+		write(2, "Mutex error\n", 12);
+	return ;
+}
+
+void	ft_error(int error, t_data *data)
+{
+	if (error == 0)
+		return ;
+	if (data)
+		free(data);
+	erro_msg(error);
+	exit(EXIT_FAILURE);
+}
+
+long	get_current_time_ms(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
 int	ft_atoi(const char *str)
@@ -32,30 +58,51 @@ int	ft_atoi(const char *str)
 	return (sign * ans);
 }
 
-void init_args(char **argv, t_philo *args)
+int	init_args(char **argv, t_data *data)
 {
+	data->qty_philo = ft_atoi(argv[1]);
+	data->t_die = ft_atoi(argv[2]);
+	data->t_eat = ft_atoi(argv[3]);
+	data->t_sleep = ft_atoi(argv[4]);
+	data->qty_eat = -1;
+	data->start_time = get_current_time_ms();
+	if (argv[5])
+		data->qty_eat = ft_atoi(argv[5]);
+	if (data->qty_philo == 0 || data->qty_eat == 0)
+		return (INVALID_ARGS);
+	if (pthread_mutex_init(&data->mutex, NULL) != 0)
+		return (MUTEX);
+	return (0);
+}
 
-	args->philo_qty = ft_atoi(argv[1]);
-	args->die_t = ft_atoi(argv[2]);
-	args->eat_t = ft_atoi(argv[3]);
-	args->sleep_t = ft_atoi(argv[4]);
-	args->eat_qty = ft_atoi(argv[5]);
-	return ;
+int	parse_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc != 5 && argc != 6)
+		return (USAGE);
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (INVALID_ARGS);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo *args;
+	t_data	data;
+	t_philo	*philo;
 
-
-	if (argc != 6)
-		return(printf("Usage:%s <number_of_philosophers> <time_to_die> \
-<time_to_eat> <time_to_sleep> <number_of_times_each_philosopher_must_eat>",
-		argv[0]));
-	args = malloc(sizeof(t_philo));
-	if (!args)
-			ft_error(42);
-	*args = (t_philo){0};
-	init_args(argv, args);
+	ft_error(parse_args(argc, argv), NULL);
+	ft_error(init_args, NULL);
 	return (0);
 }
